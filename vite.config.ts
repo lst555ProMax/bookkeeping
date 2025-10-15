b214026033f82e81594566db304674265b94a718
lst555ProMax <2906731850@qq.com>
2025-10-15 20:22:16 +0800

    feat: 为部署的github做准备

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/bookkeeping/', // GitHub Pages 部署路径
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
